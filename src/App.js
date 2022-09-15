912b9e5625a7e347ac674aa322ad6577e0add2a9
gmoralrubio <gmoralrubio@gmail.com>
2022-09-15 17:13:18 +0200

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,33 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import List from './pages/List'
+import New from './pages/New'
+import Single from './pages/Single'
+
 function App() {
-  return <div>app</div>
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route index element={<Home />} />
+            <Route path="login" element={<Login />} />
+            <Route path="users">
+              <Route index element={<List />} />
+              <Route path=":userId" element={<Single />} />
+              <Route path="new" element={<New />} />
+            </Route>
+            <Route path="products">
+              <Route index element={<List />} />
+              <Route path=":productId" element={<Single />} />
+              <Route path="new" element={<New />} />
+            </Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
 }
 
 export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>React Admin Panel</h1>
+    </div>
+  )
+}
